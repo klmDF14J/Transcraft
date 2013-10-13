025892bd0e8a52132c3cf81d161e79b54aaaf733
CodedModding <codedmodding@gmail.com>
2013-10-13 13:10:18 +0100

    Just sorting out

--- common/mark123mark/mods/transcraft/Transcraft.java
+++ common/mark123mark/mods/transcraft/Transcraft.java
@@ -51,7 +51,7 @@ import cpw.mods.fml.common.registry.LanguageRegistry;
 
 public class Transcraft {
 	public static final String VERSION = "1.6.4_V1.0.0_";
-	public static final String STATE = "Prerelease_4";
+	public static final String STATE = "Release_1";
 	public static boolean SHOWDEVMESSGAE = false;
 	public static boolean TransmatterFurnace = true;
 
