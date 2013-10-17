1d534cf715221b0113edf6e71a0a1b0ce3796681
codedmodding <codedmodding@gmail.com>
2013-10-17 11:11:31 +0100

    Fixed Gui Handler and added new gui 'Toby

--- common/mark123mark/mods/transcraft/Items/TransTab/GuiTransTab.java
+++ common/mark123mark/mods/transcraft/Items/TransTab/GuiTransTab.java
@@ -1,8 +1,13 @@
 package mark123mark.mods.transcraft.Items.TransTab;
 
 import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.world.World;
 
 public class GuiTransTab extends GuiScreen
 {
-
+	public GuiTransTab(EntityPlayer p, World w)
+	{
+		
+	}
 }
