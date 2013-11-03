ffba7ff9ca28eee441dca6ab5796c918671adb89
CodedModding <codedmodding@gmail.com>
2013-11-03 01:23:56 +0000

    Made world gen work in transmania!

--- common/mark123mark/mods/transcraft/TranscraftGenerator.java
+++ common/mark123mark/mods/transcraft/TranscraftGenerator.java
@@ -28,6 +28,9 @@ public class TranscraftGenerator implements IWorldGenerator {
 		case 1:
 		    generateEnd(world, random, chunkX * 16, chunkZ * 16);
 		    break;
+		case 234:
+		    generateTranDim(world, random, chunkX * 16, chunkZ * 16);
+		    break;
 		}
 		
 	}
@@ -78,6 +81,47 @@ public class TranscraftGenerator implements IWorldGenerator {
 		
 	}
 			
+private void generateTranDim(World world, Random random, int i, int j) {
+        
+		for(int k = 0; k < 7; k++) {
+			int TranscraftOreXCoord = i + random.nextInt(16);
+			int TranscraftOreYCoord = random.nextInt(32);
+			int TranscraftOreZCoord = j + random.nextInt(16);
+			
+			(new WorldGenMinable(Config.TranscraftOreID, 7)).generate(world, random, TranscraftOreXCoord, TranscraftOreYCoord, TranscraftOreZCoord);
+			
+		}
+		for(int k = 0; k < 10; k++) {
+			int TranscraftOreXCoord = i + random.nextInt(16);
+			int TranscraftOreYCoord = random.nextInt(64);
+			int TranscraftOreZCoord = j + random.nextInt(16);
+			
+			(new WorldGenMinable(Config.OilOreID, 10)).generate(world, random, TranscraftOreXCoord, TranscraftOreYCoord, TranscraftOreZCoord);
+		}
+
+		{
+			if(random.nextInt(3500) == 0)
+			{
+				new WorldGenRingSphere(world, random).generate(i + 8, 0, j + 8);
+			}
+			if(random.nextInt(1500) == 0)
+			{
+				new WorldGenRings(world, random).generate(i + 8, 0, j + 8);
+			}
+			if(random.nextInt(1500) == 0)
+			{
+				new WorldGenRuins(world, random).generate(i + 8, 0, j + 8);
+			}
+			if(random.nextInt(1750) == 0)
+			{
+				new WorldGenTower(world, random).generate(i + 8, 0, j + 8);
+			}
+		}
+		
+		
+		
+	}
+	
 	private void generateNether(World world, Random random, int i, int j) {}
 	
 	
