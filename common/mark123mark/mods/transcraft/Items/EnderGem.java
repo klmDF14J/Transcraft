f69187b7fff3225179eb37aeb13ca52cf85f7c9c
modmuss50 <m123world@gmail.com>
2013-11-23 20:58:45 +0000

    you guys will love this!
    EPIC pcs only!

--- common/mark123mark/mods/transcraft/Items/EnderGem.java
+++ common/mark123mark/mods/transcraft/Items/EnderGem.java
@@ -2,9 +2,15 @@ package mark123mark.mods.transcraft.Items;
 
 
 
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.texture.IconRegister;
+import net.minecraft.entity.passive.EntityChicken;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumMovingObjectType;
 import net.minecraft.util.Icon;
+import net.minecraft.world.World;
 
 public class EnderGem extends Item {
 
@@ -28,7 +34,22 @@ public class EnderGem extends Item {
 			 itemIcon = par1IconRegister.registerIcon("Transcraft:EnderGem");
 		}
 	
-	
+	 @Override
+	 public ItemStack onItemRightClick(ItemStack i, World w, EntityPlayer p)
+	 {
+		 
+	 if(Minecraft.getMinecraft().objectMouseOver != null)
+	  {
+	   if(Minecraft.getMinecraft().objectMouseOver.typeOfHit == EnumMovingObjectType.ENTITY)
+	   {
+	 //   if(Minecraft.getMinecraft().objectMouseOver.entityHit instanceof EntityChicken)
+	   // {
+	     w.createExplosion(Minecraft.getMinecraft().objectMouseOver.entityHit, Minecraft.getMinecraft().objectMouseOver.entityHit.posX, Minecraft.getMinecraft().objectMouseOver.entityHit.posY, Minecraft.getMinecraft().objectMouseOver.entityHit.posZ, 30F, true);
+	  //  }
+	   }
+	  }
+	return i;
+	 }
   
 	
 }
