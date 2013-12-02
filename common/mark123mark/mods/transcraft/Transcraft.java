d9bb3824f945798808b7b9052c21de62fba06e07
modmuss50 <m123world@gmail.com>
2013-12-02 20:45:05 +0000

    stopped transland spawning

--- common/mark123mark/mods/transcraft/Transcraft.java
+++ common/mark123mark/mods/transcraft/Transcraft.java
@@ -187,7 +187,7 @@ public class Transcraft
 		BiomeManager.addSpawnBiome(TransmutterBiome);
 		BiomeManager.addStrongholdBiome(TransmutterBiome);
 
-		GameRegistry.addBiome(FlatLands);
+		// GameRegistry.addBiome(FlatLands);
 
 	}
 
