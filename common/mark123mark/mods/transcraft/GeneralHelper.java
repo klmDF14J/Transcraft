509e4b9324940d0e3e3bd69dbb3ea87dbd9dc9a8
modmuss50 <m123world@gmail.com>
2013-12-13 19:36:04 +0000

    cleaned up

--- common/mark123mark/mods/transcraft/GeneralHelper.java
+++ common/mark123mark/mods/transcraft/GeneralHelper.java
@@ -1,28 +1,12 @@
 package mark123mark.mods.transcraft;
 
-import java.util.ArrayList;
-
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.monster.IMob;
-import net.minecraft.item.ItemStack;
 
 public class GeneralHelper
 {
 
-	public static Object[] convertSingleStackToPluralStacks(ItemStack stack)
-	{
 
-		ArrayList<ItemStack> list = new ArrayList<ItemStack>();
-		ItemStack currentStack;
-
-		for (int i = 0; i < stack.stackSize; i++)
-		{
-			currentStack = new ItemStack(stack.itemID, 1, stack.getItemDamage());
-			list.add(currentStack);
-		}
-
-		return list.toArray();
-	}
 
 	public static boolean isHostileEntity(EntityLivingBase entity)
 	{
