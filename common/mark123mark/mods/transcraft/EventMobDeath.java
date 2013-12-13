f8af3d6875a93c4ba597e6123b4cf5714dab6568
modmuss50 <m123world@gmail.com>
2013-12-13 19:35:20 +0000

    mobs now drop basic transmutter

--- common/mark123mark/mods/transcraft/EventMobDeath.java
+++ common/mark123mark/mods/transcraft/EventMobDeath.java
@@ -3,7 +3,6 @@ package mark123mark.mods.transcraft;
 import java.util.Random;
 
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.entity.projectile.EntityArrow;
 import net.minecraftforge.event.ForgeSubscribe;
 import net.minecraftforge.event.entity.living.LivingDropsEvent;
 
@@ -11,22 +10,15 @@ public class EventMobDeath
 {
 
 	public static double rand;
-	public Random r = new Random();
+	public Random random = new Random();
 
 	@ForgeSubscribe
 	public void onEntityDrop(LivingDropsEvent event)
 	{
 
-		if (event.source.getSourceOfDamage() instanceof EntityArrow)
-		{
-			if (((EntityArrow) event.source.getSourceOfDamage()).shootingEntity != null)
-			{
-				if (((EntityArrow) event.source.getSourceOfDamage()).shootingEntity instanceof EntityPlayer)
-				{
-					ItemHelper.droparrow((EntityPlayer) ((EntityArrow) event.source.getSourceOfDamage()).shootingEntity, event.entityLiving);
-				}
-			}
-		}
+		if (event.source.getDamageType().equals("player")) {
+            ItemHelper.droptransmutter((EntityPlayer) event.source.getSourceOfDamage(), event.entityLiving);
+        }
 	}
 
 }
