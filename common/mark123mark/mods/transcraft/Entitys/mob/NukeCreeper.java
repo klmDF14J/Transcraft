3609d575e41e045c62090d82ab9cf0dd2c79363a
modmuss50 <m123world@gmail.com>
2013-12-14 12:35:38 +0000

    removed ender creeper teleporting

--- common/mark123mark/mods/transcraft/Entitys/mob/NukeCreeper.java
+++ common/mark123mark/mods/transcraft/Entitys/mob/NukeCreeper.java
@@ -1,7 +1,6 @@
 package mark123mark.mods.transcraft.Entitys.mob;
 
 import mark123mark.mods.transcraft.Transcraft;
-import net.minecraft.block.Block;
 import net.minecraft.entity.ai.EntityAIAttackOnCollide;
 import net.minecraft.entity.ai.EntityAIWander;
 import net.minecraft.entity.monster.EntityCreeper;
@@ -9,10 +8,7 @@ import net.minecraft.entity.monster.EntityIronGolem;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.DamageSource;
 import net.minecraft.util.EntityDamageSource;
-import net.minecraft.util.MathHelper;
 import net.minecraft.world.World;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.entity.living.EnderTeleportEvent;
 
 public class NukeCreeper extends EntityCreeper
 {
@@ -202,97 +198,6 @@ public class NukeCreeper extends EntityCreeper
 	protected int getDropItemId()
 	{
 		return Transcraft.DarkEndershard.itemID;
-	}
-
-	/**
-	 * Teleport the enderman to a random nearby position
-	 */
-	protected boolean teleportRandomly()
-	{
-		double d0 = this.posX + (this.rand.nextDouble() - 0.5D) * 64.0D;
-		double d1 = this.posY + (this.rand.nextInt(64) - 32);
-		double d2 = this.posZ + (this.rand.nextDouble() - 0.5D) * 64.0D;
-		return this.teleportTo(d0, d1, d2);
-	}
-
-	/**
-	 * Teleport the enderman
-	 */
-	protected boolean teleportTo(double par1, double par3, double par5)
-	{
-		EnderTeleportEvent event = new EnderTeleportEvent(this, par1, par3, par5, 0);
-		if (MinecraftForge.EVENT_BUS.post(event))
-		{
-			return false;
-		}
-
-		double d3 = this.posX;
-		double d4 = this.posY;
-		double d5 = this.posZ;
-		this.posX = event.targetX;
-		this.posY = event.targetY;
-		this.posZ = event.targetZ;
-		boolean flag = false;
-		int i = MathHelper.floor_double(this.posX);
-		int j = MathHelper.floor_double(this.posY);
-		int k = MathHelper.floor_double(this.posZ);
-		int l;
-
-		if (this.worldObj.blockExists(i, j, k))
-		{
-			boolean flag1 = false;
-
-			while (!flag1 && j > 0)
-			{
-				l = this.worldObj.getBlockId(i, j - 1, k);
-
-				if (l != 0 && Block.blocksList[l].blockMaterial.blocksMovement())
-				{
-					flag1 = true;
-				}
-				else
-				{
-					--this.posY;
-					--j;
-				}
-			}
-
-			if (flag1)
-			{
-				this.setPosition(this.posX, this.posY, this.posZ);
-
-				if (this.worldObj.getCollidingBoundingBoxes(this, this.boundingBox).isEmpty() && !this.worldObj.isAnyLiquid(this.boundingBox))
-				{
-					flag = true;
-				}
-			}
-		}
-
-		if (!flag)
-		{
-			this.setPosition(d3, d4, d5);
-			return false;
-		}
-		else
-		{
-			short short1 = 128;
-
-			for (l = 0; l < short1; ++l)
-			{
-				double d6 = l / (short1 - 1.0D);
-				float f = (this.rand.nextFloat() - 0.5F) * 0.2F;
-				float f1 = (this.rand.nextFloat() - 0.5F) * 0.2F;
-				float f2 = (this.rand.nextFloat() - 0.5F) * 0.2F;
-				double d7 = d3 + (this.posX - d3) * d6 + (this.rand.nextDouble() - 0.5D) * this.width * 2.0D;
-				double d8 = d4 + (this.posY - d4) * d6 + this.rand.nextDouble() * this.height;
-				double d9 = d5 + (this.posZ - d5) * d6 + (this.rand.nextDouble() - 0.5D) * this.width * 2.0D;
-				this.worldObj.spawnParticle("portal", d7, d8, d9, f, f1, f2);
-			}
-
-			this.worldObj.playSoundEffect(d3, d4, d5, "mob.endermen.portal", 1.0F, 1.0F);
-			this.playSound("mob.endermen.portal", 1.0F, 1.0F);
-			return true;
-		}
 	}
 
 }
