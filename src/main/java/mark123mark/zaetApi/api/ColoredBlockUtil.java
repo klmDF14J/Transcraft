d2a1385016c9eced132511ec104557e6ea8831b8
modmuss50 <m123world@gmail.com>
2013-12-29 16:18:06 +0000

    updated zate api

--- src/main/java/mark123mark/zaetApi/api/ColoredBlockUtil.java
+++ src/main/java/mark123mark/zaetApi/api/ColoredBlockUtil.java
@@ -1,37 +1,37 @@
-package mark123mark.zaetApi.api;
+package org.zaet.api;
 
 import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
 import cpw.mods.fml.common.registry.LanguageRegistry;
 
-public class ColoredBlockUtil {
+public class ColoredBlockUtil
+{
 	/***
 	 * 
-	 * @param itemBlock
-	 *            The ItemBlock
-	 * @param suffix
-	 *            Red $SUFFIX$
+	 * @param itemBlock The ItemBlock
+	 * @param suffix Red $SUFFIX$
 	 * 
-	 *            The method to add localizations to IColoredItemBlocks
+	 * The method to add localizations to IColoredItemBlocks
 	 */
-	public static void addLocalizationsForBlock(ItemBlock itemBlock,
-			String suffix) {
-		for (int i = 15; i >= 0; i--) {
-			LanguageRegistry.instance().addStringLocalization(
-					itemBlock.getUnlocalizedName(new ItemStack(
-							itemBlock, 1, i)) + ".name",
-					getColorNameByMetadata(i) + " " + suffix);
+	public static void addLocalizationsForBlock(ItemBlock itemBlock, String suffix)
+	{
+		for(int i = 15; i >= 0; i--)
+		{
+			System.out.println(itemBlock.getUnlocalizedName(new ItemStack(itemBlock, 1, i)));
+			LanguageRegistry.instance().addStringLocalization(itemBlock.getUnlocalizedName(new ItemStack(itemBlock, 1, i)) + ".name", getColorNameByMetadata(i) + " " + suffix);
+			LanguageRegistry.addName(itemBlock, getColorNameByMetadata(i) + " " + suffix);
 		}
 	}
-
+	
 	/**
 	 * 
 	 * @param Meta
 	 * @return The Color from Meta
 	 * 
-	 *         Gets a color from a meta-data file.
+	 * Gets a color from a meta-data file.
 	 */
-	public static String getColorNameByMetadata(int meta) {
+	public static String getColorNameByMetadata(int meta)
+	{
 		return IColoredItemBlock.subNames[meta];
 	}
 }
